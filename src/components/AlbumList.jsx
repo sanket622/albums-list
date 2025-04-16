63238b1c9b686f42738dd7c2fb364211b9e8260c
sanket622 <sanketkumar0068@gmail.com>
2025-04-16 11:54:30 +0530

    update

--- src/components/AlbumList.jsx
+++ src/components/AlbumList.jsx
@@ -1,14 +1,18 @@
+// AlbumList.jsx
+import './AlbumList.css';
+
 export default function AlbumList({ albums, onDelete, onEdit }) {
-    return (
-      <ul className="album-list">
-        {albums.map((album) => (
-          <li key={album.id}>
-            {album.title}
-            <button onClick={() => onEdit(album)}>Edit</button>
-            <button onClick={() => onDelete(album.id)}>Delete</button>
-          </li>
-        ))}
-      </ul>
-    );
-  }
-  
+  return (
+    <ul className="album-list">
+      {albums.map((album) => (
+        <li key={album.id} className="album-item">
+          <span className="album-title">{album.title}</span>
+          <div className="button-group">
+            <button onClick={() => onEdit(album)} className="edit-btn">Edit</button>
+            <button onClick={() => onDelete(album.id)} className="delete-btn">Delete</button>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
